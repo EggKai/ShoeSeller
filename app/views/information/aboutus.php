41cffe01966c94af1e3f033b3c014bc275d65b2f
Shun Ren <shunrenchang@gmail.com>
2025-03-19 13:52:44 +0800

    Updated About page

--- app/views/information/aboutus.php
+++ app/views/information/aboutus.php
@@ -1,21 +1,44 @@
+<head>
 <?php
 $title = 'About Us';
 include __DIR__ . '/../inc/header.php';
 ?>
-<div class="__content">
+<link rel="stylesheet"  href="public\assets\css\aboutus.css">
+</head>
 <section class="about-container">
-    <h2>Welcome to Shoe Store!</h2>
-    <p>We are dedicated to providing high-quality footwear with the latest trends and styles. Our goal is to bring
-        comfort, durability, and fashion together in every pair.</p>
-    <h3>Why Choose Us?</h3>
+<h1>ABOUT US</h1>
+    <div class = 'top-section'>    
+    <p>At Shoeseller, we exist to unite the sneaker community.
+It's not just about the shoes we wear, but the passion we share for movement and expression. 
+Our paths may be different, but the drive to push limits and embrace style is what brings us together. 
+Because at the end of the day, we’re more than just a brand – we’re a team of individuals who know that
+to go further, we go together.
+Born from humble beginnings, Shoeseller started with a dream and a passion for sneakers.
+Today, we craft the footwear that fuels your every step, the culture that inspires your journey,
+and the community where you can express your personal style.
+Shoeseller is more than just a store – it's a movement. Join us, and step into something bigger.</p>
+<img src="public\assets\images\model.jpg" alt="model">
+</div>
+
+    <h1>Why Choose Us?</h1>
     <ul>
-        <li>✔ High-quality materials</li>
-        <li>✔ Affordable prices</li>
-        <li>✔ Fast & reliable delivery</li>
-        <li>✔ Excellent customer service</li>
+        <li> High-quality materials</li>
+        <li> Affordable prices</li>
+        <li> Fast & reliable delivery</li>
+        <li> Excellent customer service</li>
+    </ul>
+
+<div class="bottom-section">
+    <img src="public\assets\images\model2.jpg" height="500" width="600" alt="sneakers">
+    <div class ='bottom-text'>
+    <h1>Our Core Values</h1>
+    <ul>
+        <li>✔ Customer Satisfaction</li>
+        <li>✔ Giving Back To Community</li>
+        <li>✔ Authenticity</li>
+        <li>✔ Staying Ahead</li>
     </ul>
 </section>
-</div>
 <?php
 include __DIR__ . '/../inc/footer.php';
 
