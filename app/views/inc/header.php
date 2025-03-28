2b9f25d76ba2ed940c308c97c198b17861fcae71
Nate-0003 <nathyn.ho@gmail.com>
2025-03-29 00:54:11 +0800

    Updated paths in header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -10,85 +10,85 @@
     <meta name="description" content=<?php echo isset($description) ? $description : "Shoe Store Page"; ?>>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo isset($title) ? $title : "Shoe Store"; ?></title>
-    <link rel="icon" type="image/x-icon" href="public/assets/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="/public/assets/images/favicon.ico">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    <link rel="stylesheet" href="public/assets/css/main.css">
-    <link rel="stylesheet" href="public/assets/css/products.css">
-    <link rel="stylesheet" href="public/assets/css/product-detail.css">
-    <link rel="stylesheet" href="public/assets/css/additional-styles.css">
-    <link rel="stylesheet" href="public/assets/css/hamburger-nav.css">
-    <script defer src="public/assets/js/hamburger-nav.js"></script>
+    <link rel="stylesheet" href="/public/assets/css/main.css">
+    <link rel="stylesheet" href="/public/assets/css/products.css">
+    <link rel="stylesheet" href="/public/assets/css/product-detail.css">
+    <link rel="stylesheet" href="/public/assets/css/additional-styles.css">
+    <link rel="stylesheet" href="/public/assets/css/hamburger-nav.css">
+    <script defer src="/public/assets/js/hamburger-nav.js"></script>
     <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
     <?php if (isset($options)) { ?> <!-- dynamic loading of assets -->
         <?php if (in_array('cart', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/cart.css">
-            <script defer src="public/assets/js/cart.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/cart.css">
+            <script defer src="/public/assets/js/cart.js"></script>
         <?php } ?>
         <?php if (in_array('carousel', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/carousel.css">
-            <script defer src="public/assets/js/carousel.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/carousel.css">
+            <script defer src="/public/assets/js/carousel.js"></script>
         <?php } ?>
         <?php if (in_array('landing', $options)) { ?>
             <script defer src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
             <script defer src="https://unpkg.com/lenis@1.1.14/dist/lenis.min.js"></script>
-            <script defer src="public/assets/js/landing.js"></script>
+            <script defer src="/public/assets/js/landing.js"></script>
         <?php } ?>
         <?php if (in_array('form', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/form.css">
-            <link rel="stylesheet" href="public/assets/css/alerts.css">
+            <link rel="stylesheet" href="/public/assets/css/form.css">
+            <link rel="stylesheet" href="/public/assets/css/alerts.css">
         <?php } ?>
         <?php if (in_array('form-carousel', $options)) { ?>
-            <script defer src="public/assets/js/form-carousel.js"></script>
+            <script defer src="/public/assets/js/form-carousel.js"></script>
         <?php } ?>
         <?php if (in_array('form-carousel-forked', $options)) { ?>
-            <script defer src="public/assets/js/form-carousel-forked.min.js"></script>
+            <script defer src="/public/assets/js/form-carousel-forked.min.js"></script>
         <?php } ?>
         <?php if (in_array('profile', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/profile.css">
+            <link rel="stylesheet" href="/public/assets/css/profile.css">
         <?php } ?>
         <?php if (in_array('checkout-form', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/checkout-form.css">
+            <link rel="stylesheet" href="/public/assets/css/checkout-form.css">
         <?php } ?>
         <?php if (in_array('addCart', $options)) { ?>
-            <script defer src="public/assets/js/add-to-cart.js"></script>
+            <script defer src="/public/assets/js/add-to-cart.js"></script>
         <?php } ?>
         <?php if (in_array('sizes-list', $options)) { ?>
-            <script defer src="public/assets/js/sizes-list.js"></script>
+            <script defer src="/public/assets/js/sizes-list.js"></script>
         <?php } ?>
         <?php if (in_array('floating-button', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/floating-button.css">
+            <link rel="stylesheet" href="/public/assets/css/floating-button.css">
         <?php } ?>
         <?php if (in_array('dashboard', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/dashboard.css">
+            <link rel="stylesheet" href="/public/assets/css/dashboard.css">
         <?php } ?>
         <?php if (in_array('view-users', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/view-users.css">
-            <script defer src="public/assets/js/deleteUsers.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/view-users.css">
+            <script defer src="/public/assets/js/deleteUsers.js"></script>
         <?php } ?>
         <?php if (in_array('aboutus', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/aboutus.css">
+            <link rel="stylesheet" href="/public/assets/css/aboutus.css">
         <?php } ?>
         <?php if (in_array('contactus', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/information.css">
+            <link rel="stylesheet" href="/public/assets/css/information.css">
         <?php } ?>
         <?php if (in_array('information', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/information.css">
-            <script defer src="public/assets/js/information.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/information.css">
+            <script defer src="/public/assets/js/information.js"></script>
         <?php } ?>
         <?php if (in_array('review', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/review.css">
+            <link rel="stylesheet" href="/public/assets/css/review.css">
         <?php } ?>
         <?php if (in_array('locations', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/locations.css">
-            <script defer src="public/assets/js/locations.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/locations.css">
+            <script defer src="/public/assets/js/locations.js"></script>
         <?php } ?>
         <?php if (in_array('view-logs', $options)) { ?>
-            <link rel="stylesheet" href="public/assets/css/logs.css">
-            <script defer src="public/assets/js/logs.js"></script>
+            <link rel="stylesheet" href="/public/assets/css/logs.css">
+            <script defer src="/public/assets/js/logs.js"></script>
         <?php } ?>
     <?php } ?>
-    <link rel="stylesheet" href="public/assets/css/footer.css">
+    <link rel="stylesheet" href="/public/assets/css/footer.css">
 </head>
 
 <body>
@@ -96,8 +96,8 @@
         <nav class="main-nav" aria-label="Main navigation">
             <ul class="nav-left desktop-nav">
                 <li>
-                    <a href="index.php">
-                        <img src="public/assets/images/logo.png" alt="Shoe Store Logo">
+                    <a href="/index.php">
+                        <img src="/public/assets/images/logo.png" alt="Shoe Store Logo">
                     </a>
                 </li>
                 <li><a href="index.php?url=products/all">Products</a></li>
